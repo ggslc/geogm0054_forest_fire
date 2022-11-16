77a7403b75f2b62d95226f1ee12e6a495775e32d
Mohammed Al Darai <pi22056@bristol.ac.uk>
2022-11-16 10:57:04 +0000

    increase timesteps

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -12,7 +12,7 @@ from ca_fire import evolve_forest, write_netcdf
 GRID_N_X = 100  # sub-divisions along X axis
 PROB_GROWTH = 1.0e-4  # probability of new growth per cell per unit time
 PROB_NEW_FIRE = 1.0e-6  # probability of new fire per cell per unit time
-N_TIME_STEP = 20000  # number of time-steps
+N_TIME_STEP = 40000  # number of time-steps
 VERBOSE = True # set to False to suppress printing
 
 # run the model
