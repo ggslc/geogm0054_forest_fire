0785b63c5427ab03218f7e884cd20137ff4c16a3
Mohammed Al Darai <pi22056@bristol.ac.uk>
2022-11-16 10:37:10 +0000

    corrected author

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  4 20:35:51 2022
+Created on Wed Oct  17 10:31:51 2022
 
-@author: steph
+@author: Mohammed Al Darai
 """
 from ca_fire import evolve_forest, write_netcdf
 
