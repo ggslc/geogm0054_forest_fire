d7f696557ad4a344ba614d2e097b874a092fb3e1
Killaspy_S <mb23893@bristol.ac.uk>
2023-12-07 12:41:02 +0000

    corrected author

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  4 20:35:51 2022
+Created on Thur Dec 07 2023 12:38 
 
-@author: steph
+@author: Killaspy_S
 """
 from ca_fire import evolve_forest, write_netcdf
 
